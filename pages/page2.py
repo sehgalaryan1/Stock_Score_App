d91b8e0b17b8d107c971d0ca394e3488be4ef490
raskirtsingh <68180087+raskirtsingh@users.noreply.github.com>
2025-04-28 18:58:41 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -96,8 +96,8 @@ def main():
             raw_sharpe = monthly_ret.mean() / monthly_ret.std() * (12**0.5)
             sharpe = float(raw_sharpe)
             
-            st.subheader("Sharpe Ratio (monthly over 2 yr)")
-            st.write(f"### {sharpe:.2f}")
+            st.subheader(f"Sharpe Ratio (over past 2 yr): {sharpe:.2f}")
+            
         except Exception as e:
             st.error(f"Error computing Sharpe Ratio: {e}")
 
