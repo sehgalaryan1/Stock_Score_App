53c6dda16dd8d54433aa7d71eba53c0af9cdff62
raskirtsingh <68180087+raskirtsingh@users.noreply.github.com>
2025-04-28 21:25:56 -0400

    Update page4.py

--- pages/page4.py
+++ pages/page4.py
@@ -6,12 +6,10 @@ def main():
     # --- How It Works Section ---
     st.header("How It Works")
     st.markdown("""
-- **Model Training Data**: WRDS Compustat (Jan 2015 – Dec 2024) containing 26K rows and using fundamental metrics like current assets, net income, liabilities, etc. and technical metrics like return, volume, and volatility. 
+- **Model Training Data**: WRDS Compustat (Jan 2015 – Dec 2024) containing 26K rows and using fundamental metrics like current assets, net income, liabilities, etc. and technical metrics like return, volume, volatility, etc. 
 - **Real time App Data**: Real-time technnical & fundamental indicators from Yahoo Finance. 
 - **Models**: Two separate scikit-learn pipelines utilizing Random Forest Regressor. 
 - **Companies for Model Training**: Top 150 companies from each of the S&P 400, S&P 500, S&P 600 indices.
-
-These are combined via user-chosen weights into a final **1–10 Investment Rating**.
     """)
 
     st.divider()
@@ -25,13 +23,13 @@ These are combined via user-chosen weights into a final **1–10 Investment Rati
 - **Technical Features** (7 numeric + 1 categorical):  
   `monthly_return, month_trading_volume, stdev, avg_ret_6m, avg_ret_12m, vol_6m, vol_12m, gics_sector_x`
 
-- **Targets**:
+- **Target Variables**:
     - Fundamental → `f_score` (Piotroski) 
     - Technical → `sharpe_ratio`
     
 - **Rating**:
-    - We used MinMax Scaler to scale our fundamental and technical scores into two seperate ratings ranging from 1-10.
-    - The ratings are then averaged out (based on the user's fundamental and technical weightage) to return the final score 
+    - MinMax Scaler scales the fundamental and technical scores into two seperate ratings ranging from 1-10.
+    - The final score is computed as a weighted average of the fundamental and technical ratings, using the user’s specified weightings. 
     """)
 
     st.divider()
